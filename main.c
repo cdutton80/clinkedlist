7e305c8668f55b1c8578a6a26c5ae3299e1c605b
Chris Dutton <67941058+cdutton80@users.noreply.github.com>
2021-07-21 13:54:12 -0700

    Update main.c

--- main.c
+++ main.c
@@ -14,10 +14,18 @@ void print_intflt_tuple(CPointer val);
 CPointer int_add(CPointer init, CPointer x);
 CPointer times2_plus1_div2(CPointer val);
 void printi_2int(CPointer v1, CPointer v2, int i);
+int cmp_int(CPointer v1, CPointer v2);
+CPointer int_mod4(CPointer val);
+
+void print_intlist(CSList * list);
+void print_int_intlist_tuple(C2Tuple * t);
 
 
 int main(void) {
-    // int zero = 0;
+    int zero = 0;
+    int one = 1;
+    int two = 2;
+    int eight = 8;
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
     CSList * lst = CSList_alloc(sizeof(int));
@@ -32,19 +40,20 @@ int main(void) {
     }
     
     CSList * lst3 = CSList_intersperse2_ref(lst2, lst);
+
+    CSList_append_ref(lst3, &one);
+    CSList_append_ref(lst3, &two);
+    CSList_append_ref(lst3, &eight);
+
+
+
+
+    CSList * u = CSList_uniq_ref(lst3, cmp_int);
+
+
+    CSList * z = CSList_group_by_ref(lst3, sizeof(int), int_mod4, cmp_int);
     
-    // CSList_iter(lst3, print_int);
-    
-    // CSList_iter2i(lst, lst2, printi_2int);
-    
-    CSList * m = CSList_find_first_n_ref(lst3, 4, gt_two);
-    
-    CSList_rev_iter(m, print_intint_tuple);
-    
-    // CSList_iter_where(lst3, print_int, gt_two);
-    
-    C2Tuple * f = CSList_find_first_ref(lst3, gt_two);
-    print_intint_tuple(f);
+    CSList_iter(z, print_int_intlist_tuple);
 }
 
 void print_int(CPointer val) {
@@ -94,4 +103,49 @@ CPointer times2_plus1_div2(CPointer val) {
 
 void printi_2int(CPointer v1, CPointer v2, int i) {
 	fprintf(stdout, "%3d: %d, %d\n", i, *(int *)v1, *(int *)v2);
+}
+
+int cmp_int(CPointer v1, CPointer v2) {
+    int i1 = *(int *)v1;
+    int i2 = *(int *)v2;
+
+    return i1 == i2 ? 0 : 
+           i1 > i2  ? 1 : -1;
+}
+
+
+CPointer int_mod4(CPointer val) {
+    int * result = malloc(sizeof(int));
+    *result = *(int *)val % 4;
+    return result;
+}
+
+void print_intlist(CSList * list) {
+    if (list == NULL) {
+        printf("(nil)");
+    }
+    else if (CSList_empty(list)) {
+        printf("[]");
+    }
+    else if (CSList_cmp_len_with(list, 1) == 0) {
+        printf("[%d]\n", *(int *)list->head->value);
+    }
+    else {
+        printf("[");
+        CSNode * cur;
+        for (cur = list->head; cur->next != NULL; cur = cur->next) {
+            printf("%d, ", *(int *)cur->value);
+        }
+
+        printf("%d]\n", *(int *)cur->value);
+    }
+}
+
+void print_int_intlist_tuple(C2Tuple * t) {
+    int i = *(int *)t->fst;
+    CSList * il = (CSList *)t->snd;
+
+    printf("(%d, ", i);
+    print_intlist(il);
+    printf(")\n");
 }
